4e4b65da0c15d2a1ee5a4d8b92b73b3e7a201c83
Jean Jacques Uzabumuhire <uzabumuhire@gmail.com>
2022-06-15 22:56:21 +0200

    Fix issue by adding a Location to Trail

--- BlazingTrails.Client/Features/Home/Trail.cs
+++ BlazingTrails.Client/Features/Home/Trail.cs
@@ -7,6 +7,7 @@
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Image { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
         public int TimeInMinutes { get; set; }
         public string TimeFormatted => $"{TimeInMinutes / 60}h {TimeInMinutes % 60}m";
         public int Length { get; set; }
