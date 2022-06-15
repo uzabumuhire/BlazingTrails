4f81dd00515b2b5f93792dd7bfa8ec09c71ef77e
Jean Jacques Uzabumuhire <uzabumuhire@gmail.com>
2022-06-15 22:04:39 +0200

    Define a class which represents a trail

---
+++ BlazingTrails.Client/Features/Home/RouteInstruction.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazingTrails.Client.Features.Home
+{
+    // Represents a waypoint on the route to help guide other walkers
+    public class RouteInstruction
+    {
+        public int Stage { get; set; }
+        public string Description { get; set; } = string.Empty;
+    }
+}
---
+++ BlazingTrails.Client/Features/Home/Trail.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazingTrails.Client.Features.Home
+{
+    // Represents a trail
+    public class Trail
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+        public int TimeInMinutes { get; set; }
+        public string TimeFormatted => $"{TimeInMinutes / 60}h {TimeInMinutes % 60}m";
+        public int Length { get; set; }
+        public IEnumerable<RouteInstruction> Route { get; set; } = Array.Empty<RouteInstruction>();
+    }
+}
